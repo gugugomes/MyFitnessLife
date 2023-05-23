7c148e9abb7b5948bc24a26d30d1989b52365fe4
gugugomes <120190910+gugugomes@users.noreply.github.com>
2023-05-23 15:36:11 -0300

    Update ImcActivity.kt
    Escondendo o telcado quando o popUp aparecer

--- app/src/main/java/dev/gustavogomes/myfitnesslife/ImcActivity.kt
+++ app/src/main/java/dev/gustavogomes/myfitnesslife/ImcActivity.kt
@@ -1,7 +1,9 @@
 package dev.gustavogomes.myfitnesslife
 
 import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -42,6 +44,9 @@ class ImcActivity : AppCompatActivity() {
         ) { _, _ -> }
         .create()
         .show()
+
+      val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+      service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
     }
   }
 
