29e5a2c8cf923e9848c317671df12157731ff3d7
gugugomes <120190910+gugugomes@users.noreply.github.com>
2023-05-23 09:50:26 -0300

    Update ImcActivity.kt
    Gerando a resposta para o usuario

--- app/src/main/java/dev/gustavogomes/myfitnesslife/ImcActivity.kt
+++ app/src/main/java/dev/gustavogomes/myfitnesslife/ImcActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 
 class ImcActivity : AppCompatActivity() {
@@ -30,6 +31,8 @@ class ImcActivity : AppCompatActivity() {
 
       val finalResult = imcCalc(weight, height)
 
+      val imcResponseId = imcResponse(finalResult)
+      Toast.makeText(this, imcResponseId, Toast.LENGTH_SHORT).show()
     }
   }
 
@@ -42,5 +45,19 @@ class ImcActivity : AppCompatActivity() {
 
   private fun imcCalc(weight: Int, height: Int): Double {
     return weight / ((height / 100.0) * (height / 100.0))
+  }
+
+  @StringRes
+  private fun imcResponse(imc: Double): Int {
+    return when {
+      imc < 15.0 -> R.string.imc_severely_low_weight
+      imc < 16.0 -> R.string.imc_very_low_weight
+      imc < 18.5 -> R.string.imc_low_weight
+      imc < 25.0 -> R.string.normal
+      imc < 30.0 -> R.string.imc_high_weight
+      imc < 35.0 -> R.string.imc_so_high_weight
+      imc < 40.0 -> R.string.imc_severely_high_weight
+      else -> R.string.imc_extreme_weight
+    }
   }
 }
