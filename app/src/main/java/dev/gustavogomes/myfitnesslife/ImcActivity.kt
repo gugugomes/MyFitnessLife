446e620a35aa24a621a0d7cad98e9fde164c5b54
gugugomes <120190910+gugugomes@users.noreply.github.com>
2023-05-23 08:50:41 -0300

    Update 0.1.3b
    Adicionando verificação para prevenir erros

--- app/src/main/java/dev/gustavogomes/myfitnesslife/ImcActivity.kt
+++ app/src/main/java/dev/gustavogomes/myfitnesslife/ImcActivity.kt
@@ -1,11 +1,46 @@
 package dev.gustavogomes.myfitnesslife
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class ImcActivity : AppCompatActivity() {
+
+  private lateinit var editWeight: EditText
+  private lateinit var editHeight: EditText
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_imc)
+
+    editWeight = findViewById(R.id.editImcWeight)
+    editHeight = findViewById(R.id.editImcHeight)
+    val buttonSend: Button = findViewById(R.id.buttonImcSend)
+
+    buttonSend.setOnClickListener {
+      if (!validate()) {
+        Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+        return@setOnClickListener
+      }
+
+      val weight = editWeight.text.toString().toInt()
+      val height = editHeight.text.toString().toInt()
+
+      val finalResult = imcCalc(weight, height)
+
+    }
+  }
+
+  private fun validate(): Boolean {
+    return (editWeight.text.toString().isNotEmpty()
+            && editHeight.text.toString().isNotEmpty()
+            && !editWeight.text.toString().startsWith("0")
+            && !editHeight.text.toString().startsWith("0"))
+  }
+
+  private fun imcCalc(weight: Int, height: Int): Double {
+    return weight / ((height / 100.0) * (height / 100.0))
   }
 }
