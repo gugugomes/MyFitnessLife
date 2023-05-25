f89234c68c9de3dd49c025c2b0842dab5df3e6c1
gugugomes <120190910+gugugomes@users.noreply.github.com>
2023-05-25 11:33:43 -0300

    Adicionando componentes reciclaveis

--- app/src/main/java/dev/gustavogomes/myfitnesslife/MainActivity.kt
+++ app/src/main/java/dev/gustavogomes/myfitnesslife/MainActivity.kt
@@ -1,23 +1,39 @@
 package dev.gustavogomes.myfitnesslife
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var buttonImc: Button
+  //  private lateinit var buttonImc: Button
+  private lateinit var rvMain: RecyclerView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    buttonImc = findViewById(R.id.buttonIMC)
+    val adapter = MainAdpter()
+    rvMain = findViewById(R.id.mainRecycleView)
+    rvMain.adapter = adapter
+    rvMain.layoutManager = LinearLayoutManager(this)
+  }
 
-    buttonImc.setOnClickListener {
-      val imcScreen = Intent(this, ImcActivity::class.java)
-      startActivity(imcScreen)
+  private inner class MainAdpter : RecyclerView.Adapter<MainViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+      val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+      return MainViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {}
+
+    override fun getItemCount(): Int {
+      return 15
     }
   }
+
+  private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
